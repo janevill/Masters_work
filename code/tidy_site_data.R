4781ffc7055655035551b8975d3f53496812b090
janevill <janevill@ncsu.edu>
2019-10-03 09:54:19 -0400

    revised code and edited datasets

--- code/tidy_site_data.R
+++ code/tidy_site_data.R
@@ -5,6 +5,19 @@ ar_q_no3 <- read_csv("data/AR_Q_no3.csv")
 fr_q_no3 <- read_csv("data/FR_Q_no3.csv")
 ur_q_no3 <- read_csv("data/UR_Q_no3.csv")
 
-gather(date, date_1,date_2,date_3,date_4,date_5,date_6,date_7,date_8,key = "date",
-       value = "q_2","q_3", "q_4", "q_5","q_6", "q_7", "q_8", "q_9", "q_10",
-       "no3_2", "no3_3", "no3_4", "no3_5", "no3_6", "no3_7","no3_8", "no3_9", "no3_10") ->tidy_ar
+ar_q_no3 %>%
+  mutate( mass_flux_mgs= (no3_mgL*q_Ls)) ->ar_data 
+ar_data %>%
+  summary(ar_data) ->stats_ar
+fr_q_no3 %>%
+  mutate( mass_flux_mgs= (no3_mgL*q_Ls)) ->fr_data
+stats_fr <- summary(fr_data)
+ur_q_no3 %>%
+  mutate( mass_flux_mgs= (no3_mgL*q_Ls)) ->ur_data
+stats_ur <- summary(ur_data)
+
+
+
+view(stats_ar)
+view(stats_fr)
+view(stats_ur)
